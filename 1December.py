17bcdb44f1487f89c09359203d6ec43e1ef9fac8
Zud <ioZudomon@gmail.com>
2021-12-03 03:09:09 +0100

    commit 3

--- 1December.py
+++ 1December.py
@@ -2,7 +2,7 @@
 
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import math
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,11 +11,28 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm 4 ?!')
+    print_hi(' 1 December !')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
-nums = tuple(map(int, fin))
+#1December_input.txt
+
+with open('1December_input.txt', 'r') as f:
+    lines = f.read().splitlines()
+    nums = tuple(map(int, lines))
+
+#misuro quanti sono gli incrementi
 tot  = sum(b > a for a, b in zip(nums, nums[1:]))
 print('Part 1:', tot)
+
+#quanti somme di incrementi sono maggiori della prima finestra di somma ?
+
+#sum_1 = sum(nums[0],nums[1],nums[2])
+sum_1 = nums[0] + nums[1] + nums[2]
+print("sum_1",sum_1)
+
+print("nums[3:]",nums[3:])
+
+tot2 = sum(b > a for a, b in zip(nums, nums[3:])) # changed nums[1:] -> nums[3:]
+print('Part 2:', tot2)
